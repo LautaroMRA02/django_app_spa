c052ba86a931099c18beda90bce700815f4a7ea7
LautaroMRA02 <lautaro.mra.2002@gmail.com>
2023-11-15 02:12:02 -0300

    [template]:

---
+++ DJANGO_APP/core/asgi.py
@@ -0,0 +1,33 @@
+"""
+ASGI config for core project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter,URLRouter
+from django.urls import re_path
+from simple_app.consumers import EchoConsumer
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+application = get_asgi_application()
+
+
+
+application = ProtocolTypeRouter({
+# Django's ASGI application to handle traditional HTTP requests
+    "http": get_asgi_application(),
+    # WebSocket handler
+    "websocket": AuthMiddlewareStack(
+    URLRouter([
+    re_path(r"^ws/echo/$", EchoConsumer.as_asgi()),
+        ])
+    ),
+})
